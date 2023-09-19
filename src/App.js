19e57dfe6417de54cab7f462f048cc984a8340e1
Shubham Kumar <shubhamkr15060@gmail.com>
2023-09-19 16:57:08 +0530

    Setup for our movie app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from "react";
-
-// Creating class based component
-class App extends React.Component{
-  render(){
-    return <h1>Class Component</h1>
-  }
+function App(){
+  return (
+    <>
+      <h1>Movie App</h1>
+    </>
+  )
 }
 
 export default App
