85aa3eb5eea49d406899e0c9dbeae4e1d9e3ab69
Shubham Kumar <shubhamkr15060@gmail.com>
2023-09-19 16:40:53 +0530

    Class Component in React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-function App() {
-  return (
-    <h1>Functional Component</h1>
-  );
+import React from "react";
+
+// Creating class based component
+class App extends React.Component{
+  render(){
+    return <h1>Class Component</h1>
+  }
 }
 
-export default App;
+export default App
