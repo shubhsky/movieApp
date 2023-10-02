c7c23b37e67f2af31d30d9fafeb7c0e898e5aeef
Shubham Kumar <shubhamkr15060@gmail.com>
2023-10-03 00:43:39 +0530

    inline styling

--- src/navbar.js
+++ src/navbar.js
@@ -5,17 +5,11 @@ class Navbar extends React.Component{
     render(){
         return(
             <>
-                <div style={{
-                    width: '100%',
-                    height: '30px',
-                    backgroundColor:'cadetblue',
-                    display: 'flex',
-                    justifyContent: 'space-between'
-                }}>
-                    <div className="title">Movie App</div>
-                    <div>
-                        <img alt='Cart Item' />
-                        <span>0</span>
+                <div style={styles.nav}>
+                    <div style={styles.title}>Movie App</div>
+                    <div style={styles.cartContainer}>
+                        <img src="https://cdn-icons-png.flaticon.com/128/891/891462.png" alt='Cart Item' style={styles.cartIcon}/>
+                        <span style={styles.cartCount}>0</span>
                     </div>
                 </div>
             </>
@@ -27,10 +21,36 @@ export default Navbar;
 
 const styles = {
     nav:{
-        width: '100%',
-        height: '30px',
-        backgroundColor:'cadetblue',
-        display: 'flex',
-        justifyContent: 'space-between'
+        height:70,
+        backgroundColor:'#4267b2',
+        display:'flex',
+        justifyContent:'space-between',
+        alignItems:'center',
+        position:'relative'
+    },
+    title:{
+        fontSize:'30px',
+        color:'#fff',
+        fontWeight:600,
+        fontFamily:'"Montserrat",sans-serif',
+        textTransform:'uppercase',
+        marginLeft:20
+    },
+    cartContainer:{
+        position:'relative',
+        cursor:'pointer'
+    },
+    cartIcon:{
+        height:48,
+        marginRight:20,
+    },
+    cartCount:{
+        backgroundColor:"orange",
+        borderRadius:'50%',
+        padding:'4px 8px',
+        position:'absolute',
+        right:10,
+        top:-5,
+        fontSize:12
     }
 }
