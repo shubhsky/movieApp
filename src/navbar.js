c159fbed6f5cc62d2a9557e894242c7b9c73fbfa
Shubham Kumar <shubhamkr15060@gmail.com>
2023-10-03 01:06:01 +0530

    using styled component for styling

--- src/navbar.js
+++ src/navbar.js
@@ -1,17 +1,57 @@
 import React from "react";
+import styled from 'styled-components';
 
+
+// Styled Component
+const Nav = styled.div`
+    height:70px;
+    background-color:cadetblue;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    position:relative
+`;
+
+const Title = styled.div`
+    font-size:30px;
+    color:white;
+    fontWeight:600;
+    font-family:"Montserrat",sans-serif;
+    text-transform:uppercase;
+    margin-left:20px
+`;
+
+const CartContainer = styled.div`
+    position:relative;
+    cursor:pointer
+`
+
+const CartIcon = styled.img`
+    height:48px;
+    margin-right:20px
+`
+
+const CartCount = styled.span`
+    background-color:orange;
+    border-radius:50%;
+    padding:4px 8px;
+    position:absolute;
+    right:10px;
+    top:-5px;
+    font-size:12px
+`
 
 class Navbar extends React.Component{
     render(){
         return(
             <>
-                <div style={styles.nav}>
-                    <div style={styles.title}>Movie App</div>
-                    <div style={styles.cartContainer}>
-                        <img src="https://cdn-icons-png.flaticon.com/128/891/891462.png" alt='Cart Item' style={styles.cartIcon}/>
-                        <span style={styles.cartCount}>0</span>
-                    </div>
-                </div>
+                <Nav>
+                    <Title>Movie App</Title>
+                    <CartContainer>
+                        <CartIcon src="https://cdn-icons-png.flaticon.com/128/891/891462.png" alt='Cart Item' />
+                        <CartCount>0</CartCount>
+                    </CartContainer>
+                </Nav>
             </>
         )
     }
@@ -19,38 +59,3 @@ class Navbar extends React.Component{
 
 export default Navbar;
 
-const styles = {
-    nav:{
-        height:70,
-        backgroundColor:'#4267b2',
-        display:'flex',
-        justifyContent:'space-between',
-        alignItems:'center',
-        position:'relative'
-    },
-    title:{
-        fontSize:'30px',
-        color:'#fff',
-        fontWeight:600,
-        fontFamily:'"Montserrat",sans-serif',
-        textTransform:'uppercase',
-        marginLeft:20
-    },
-    cartContainer:{
-        position:'relative',
-        cursor:'pointer'
-    },
-    cartIcon:{
-        height:48,
-        marginRight:20,
-    },
-    cartCount:{
-        backgroundColor:"orange",
-        borderRadius:'50%',
-        padding:'4px 8px',
-        position:'absolute',
-        right:10,
-        top:-5,
-        fontSize:12
-    }
-}
